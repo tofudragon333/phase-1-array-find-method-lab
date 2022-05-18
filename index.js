7697f2d819d9141311a1d63ae8988bbc922180ad
Ashley Wong <tofudragon333@gmail.com>
2022-05-18 10:22:42 -0400

    commit tree 357a129838b175242c0d0ddd169f2eff8730d99c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+  function superbowlWin(record) {
+      let winYear = record.find(record => record.result === "W");
+     if (winYear === undefined) {
+         return winYear;
+     } else {
+         return winYear.year;
+     }
+  }
+
+
